85a2e016679720accbe8f88cb87d46d2459521cf
Josh Pollock <jpollock412@gmail.com>
2016-02-25 19:09:16 -0500

    refactor loading to use core class in lib for most of main class.  #54

--- google-maps-builder.php
+++ google-maps-builder.php
@@ -44,310 +44,143 @@ if ( ! defined( 'GMB_PLUGIN_FILE' ) ) {
 }
 
 
-/*----------------------------------------------------------------------------*
- * Public-Facing Functionality
- *----------------------------------------------------------------------------*/
+if ( ! class_exists( 'Google_Maps_Builder' ) ) :
 
-/*
- * Get instances of Google_Maps_Builder class
- */
-//add_action( 'plugins_loaded', array( 'Google_Maps_Builder', 'get_instance' ) );
-//add_action( 'plugins_loaded', array( 'Google_Maps_Builder_Engine', 'get_instance' ) );
-
-if ( ! class_exists( 'Google_Maps_Builder' ) ) : /**
- * Main Maps Builder Class
- *
- * @since 2.0
- */ {
-	final class Google_Maps_Builder {
-
-		/** Singleton *************************************************************/
+	/**
+	 * Load plugin if core lib is present
+	 */
+	if( ! file_exists( GMB_PLUGIN_PATH . 'vendor/wordimpress/maps-builder-core/core.php' ) ) {
+		add_action( 'admin_notices', 'gmb_no_core_lib' );
 
 		/**
-		 * @var Google_Maps_Builder The one true Google Maps Builder instance
+		 * Print admin notice if no dependencies
+		 *
+		 * @uses "admin_notice" hook
+		 */
+		function gmb_no_core_lib(){
+			printf( '<div class="notice notice-error"><p>%s</p></div>', esc_html__( 'Your install of Maps Builder is missing its Composer dependencies and can not load.', 'maps-builder-pro' ) );
+		}
+	}else {
+		require_once GMB_PLUGIN_PATH . 'vendor/wordimpress/maps-builder-core/core.php';
+
+		/**
+		 * Main Maps Builder Class
+		 *
 		 * @since 2.0
 		 */
-		private static $instance;
+		final class Google_Maps_Builder extends Google_Maps_Builder_Core {
 
 
-		/**
-		 * Unique identifier for plugin.
-		 *
-		 * The variable name is used as the text domain when internationalizing strings
-		 * of text. Its value should match the Text Domain file header in the main
-		 * plugin file.
-		 *
-		 * @since    1.0.0
-		 *
-		 * @var      string
-		 */
-		protected $plugin_slug = 'google-maps-builder';
+			/**
+			 * @var Google_Maps_Builder The one true Google Maps Builder instance
+			 * @since 2.0
+			 */
+			private static $instance;
 
+			/**
+			 * Prevent new instances
+			 */
+			private function __construct() {
+				//you can not haz instance
+			}
 
-		/**
-		 * Activation Object
-		 *
-		 * @var Google_Maps_Builder_Activate
-		 * @since 2.0
-		 */
-		public $activate;
+			/**
+			 * User meta key for marking welcome message as dismissed
+			 *
+			 * @since 2.1.0
+			 *
+			 * @var string
+			 */
+			protected $hide_welcome_key = 'gmb_hide_pro_welcome';
 
+			/**
+			 * Main Google_Maps_Builder Instance
+			 *
+			 * Insures that only one instance of Google_Maps_Builder exists in memory at any one
+			 * time. Also prevents needing to define globals all over the place.
+			 *
+			 * @since     2.0
+			 * @static
+			 * @static    var array $instance
+			 * @uses      Google_Maps_Builder::setup_constants() Setup the constants needed
+			 * @uses      Google_Maps_Builder::includes() Include the required files
+			 * @uses      Google_Maps_Builder::load_textdomain() load the language files
+			 * @see       Google_Maps_Builder()
+			 * @return    Google_Maps_Builder
+			 */
+			public static function instance() {
+				if ( ! isset( self::$instance ) && ! ( self::$instance instanceof Google_Maps_Builder ) ) {
 
-		/**
-		 * GMB Scripts Object
-		 *
-		 * @var Google_Maps_Builder_Scripts
-		 * @since 2.0
-		 */
-		public $scripts;
+					self::$instance = new Google_Maps_Builder();
 
-		/**
-		 * GMB Settings Object
-		 *
-		 * @var Google_Maps_Builder_Settings
-		 * @since 2.0
-		 */
-		public $settings;
+					add_action( 'plugins_loaded', array( self::$instance, 'load_textdomain' ) );
 
-		/**
-		 * GMB Engine Object
-		 *
-		 * @var Google_Maps_Builder_Engine
-		 * @since 2.0
-		 */
-		public $engine;
+					self::$instance->includes();
+					self::$instance->activate = new Google_Maps_Builder_Activate();
+					self::$instance->scripts  = new Google_Maps_Builder_Scripts();
+					self::$instance->settings = new Google_Maps_Builder_Settings();
+					self::$instance->engine   = new Google_Maps_Builder_Engine();
+					self::$instance->html     = new Google_Maps_Builder_HTML_Elements();
 
-		/**
-		 * GMB Plugin Meta
-		 *
-		 * @var array
-		 * @since 2.0
-		 */
-		public $meta;
+					register_activation_hook( __FILE__, array(
+						self::$instance->activate,
+						'activation_flush_rewrites'
+					) );
 
-		/**
-		 * GMB HTML elements
-		 *
-		 * @var Google_Maps_Builder_HTML_Elements
-		 * @since 2.0
-		 */
-		public $html;
+					// Read plugin meta
+					// Check that function get_plugin_data exists
+					if ( ! function_exists( 'get_plugin_data' ) ) {
+						require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+					}
+					self::$instance->meta = get_plugin_data( GMB_PLUGIN_FILE, false );
 
-		/**
-		 * User meta key for marking welcome message as dismissed
-		 *
-		 * @since 2.1.0
-		 *
-		 * @var string
-		 */
-		protected $hide_welcome_key = 'gmb_hide_pro_welcome';
-
-		/**
-		 * Main Google_Maps_Builder Instance
-		 *
-		 * Insures that only one instance of Google_Maps_Builder exists in memory at any one
-		 * time. Also prevents needing to define globals all over the place.
-		 *
-		 * @since     2.0
-		 * @static
-		 * @static    var array $instance
-		 * @uses      Google_Maps_Builder::setup_constants() Setup the constants needed
-		 * @uses      Google_Maps_Builder::includes() Include the required files
-		 * @uses      Google_Maps_Builder::load_textdomain() load the language files
-		 * @see       Google_Maps_Builder()
-		 * @return    Google_Maps_Builder
-		 */
-		public static function instance() {
-			if ( ! isset( self::$instance ) && ! ( self::$instance instanceof Google_Maps_Builder ) ) {
-
-				self::$instance = new Google_Maps_Builder();
-
-				add_action( 'plugins_loaded', array( self::$instance, 'load_textdomain' ) );
-
-				self::$instance->includes();
-				self::$instance->activate = new Google_Maps_Builder_Activate();
-				self::$instance->scripts  = new Google_Maps_Builder_Scripts();
-				self::$instance->settings = new Google_Maps_Builder_Settings();
-				self::$instance->engine   = new Google_Maps_Builder_Engine();
-				self::$instance->html     = new Google_Maps_Builder_HTML_Elements();
-
-				register_activation_hook( __FILE__, array( self::$instance->activate, 'activation_flush_rewrites' ) );
-
-				// Read plugin meta
-				// Check that function get_plugin_data exists
-				if ( ! function_exists( 'get_plugin_data' ) ) {
-					require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 				}
-				self::$instance->meta = get_plugin_data( GMB_PLUGIN_FILE, false );
+
+				return self::$instance;
+			}
+
+
+			/**
+			 * Include required files
+			 *
+			 * @access protected
+			 * @since  2.0
+			 * @return void
+			 */
+			protected function includes() {
+				$this->include_core_classes();
+				$this->load_activate();
+
+				$this->cmb2_load();
+				$this->load_files();
+				require_once GMB_PLUGIN_PATH . 'includes/class-gmb-scripts.php';
+
+				require_once GMB_PLUGIN_PATH . 'includes/class-gmb-license-handler.php';
+				require_once GMB_PLUGIN_PATH . 'includes/class-gmb-html-elements.php';
+
+				require_once GMB_PLUGIN_PATH . 'includes/admin/mashups/class-gmb-mashups-builder.php';
+
+				if ( is_admin() || ( defined( 'WP_CLI' ) && WP_CLI ) ) {
+
+					$this->load_admin();
+					//Admin
+					require_once GMB_PLUGIN_PATH . 'includes/admin/custom-field-types/cmb2-field-directions.php';
+					require_once GMB_PLUGIN_PATH . 'includes/admin/custom-field-types/cmb2-field-select-posttype.php';
+					require_once GMB_PLUGIN_PATH . 'includes/admin/custom-field-types/cmb2-field-multicheck-posttype.php';
+
+					//@TODO only load when needed
+					$this->init_map_editor_admin();
+
+					require_once GMB_PLUGIN_PATH . 'includes/admin/mashups/class-gmb-mashups-metabox.php';
+					require_once GMB_PLUGIN_PATH . 'includes/admin/import-export/class-gmb-import-export.php';
+
+				}
 
 			}
 
-			return self::$instance;
-		}
-
-		/**
-		 * Throw error on object clone
-		 *
-		 * The whole idea of the singleton design pattern is that there is a single
-		 * object, therefore we don't want the object to be cloned.
-		 *
-		 * @since  2.0
-		 * @access protected
-		 * @return void
-		 */
-		public function __clone() {
-			// Cloning instances of the class is forbidden
-			_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'gmb' ), '2.0' );
-		}
-
-		/**
-		 * Disable unserializing of the class
-		 *
-		 * @since  2.0
-		 * @access protected
-		 * @return void
-		 */
-		public function __wakeup() {
-			// Unserializing instances of the class is forbidden
-			_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'gmb' ), '2.0' );
-		}
-
-		/**
-		 * Include required files
-		 *
-		 * @access private
-		 * @since  2.0
-		 * @return void
-		 */
-		private function includes() {
-
-			Google_Maps_Builder_Core::load_activate();
-
-			Google_Maps_Builder_Core::cmb2_load();
-			Google_Maps_Builder_Core::load_files();
-			require_once GMB_PLUGIN_PATH . 'includes/class-gmb-scripts.php';
-
-			require_once GMB_PLUGIN_PATH . 'includes/class-gmb-license-handler.php';
-			require_once GMB_PLUGIN_PATH . 'includes/class-gmb-html-elements.php';
-
-			require_once GMB_PLUGIN_PATH . 'includes/admin/mashups/class-gmb-mashups-builder.php';
-
-			if ( is_admin() || ( defined( 'WP_CLI' ) && WP_CLI ) ) {
-
-				Google_Maps_Builder_Core::load_admin();
-				//Admin
-				require_once GMB_PLUGIN_PATH . 'includes/admin/custom-field-types/cmb2-field-directions.php';
-				require_once GMB_PLUGIN_PATH . 'includes/admin/custom-field-types/cmb2-field-select-posttype.php';
-				require_once GMB_PLUGIN_PATH . 'includes/admin/custom-field-types/cmb2-field-multicheck-posttype.php';
-
-				//@TODO only load when needed
-				Google_Maps_Builder_Core::init_map_editor_admin();
-
-				require_once GMB_PLUGIN_PATH . 'includes/admin/mashups/class-gmb-mashups-metabox.php';
-				require_once GMB_PLUGIN_PATH . 'includes/admin/import-export/class-gmb-import-export.php';
-
-			}
-
-		}
-
-		/**
-		 * Loads the plugin language files
-		 *
-		 * @access public
-		 * @since  2.0
-		 * @return void
-		 */
-		public function load_textdomain() {
-			// Set filter for Give's languages directory
-			$gmb_lang_dir = dirname( plugin_basename( GMB_PLUGIN_FILE ) ) . '/languages/';
-			$gmb_lang_dir = apply_filters( 'gmb_languages_directory', $gmb_lang_dir );
-
-			// Traditional WordPress plugin locale filter
-			$locale = apply_filters( 'plugin_locale', get_locale(), 'gmb' );
-			$mofile = sprintf( '%1$s-%2$s.mo', 'gmb', $locale );
-
-			// Setup paths to current locale file
-			$mofile_local  = $gmb_lang_dir . $mofile;
-			$mofile_global = WP_LANG_DIR . '/gmb/' . $mofile;
-
-			if ( file_exists( $mofile_global ) ) {
-				// Look in global /wp-content/languages/gmb folder
-				load_textdomain( 'gmb', $mofile_global );
-			} elseif ( file_exists( $mofile_local ) ) {
-				// Look in local /wp-content/plugins/gmb/languages/ folder
-				load_textdomain( 'gmb', $mofile_local );
-			} else {
-				// Load the default language files
-				load_plugin_textdomain( 'gmb', false, $gmb_lang_dir );
-			}
-		}
-
-
-		/**
-		 * Return the plugin slug.
-		 *
-		 * @since    1.0.0
-		 *
-		 * @return   string.
-		 */
-		public function get_plugin_slug() {
-			return $this->plugin_slug;
-		}
-
-		/**
-		 * Get the user meta key for marking welcome message as dismissed
-		 *
-		 * @since 2.1.0
-		 *
-		 * @return string
-		 */
-		public function get_hide_welcome_key(){
-			return $this->hide_welcome_key;
-		}
-
-		/**
-		 * Registers the Google Maps Builder Widget.
-		 *
-		 * @since 2.1.0
-		 * @return void
-		 */
-		public function init_widget() {
-			register_widget( 'Google_Maps_Builder_Widget' );
 		}
 
 	}
-}
 
 endif; // End if class_exists check
-
-
-/**
- * The main function responsible for returning the one true Maps Builder
- * Instance to functions everywhere.
- *
- * Use this function like you would a global variable, except without needing
- * to declare the global.
- *
- * Example: <?php $gmb = Google_Maps_Builder(); ?>
- *
- * @since 2.0
- * @return object|Google_Maps_Builder
- */
-function Google_Maps_Builder() {
-	return Google_Maps_Builder::instance();
-}
-
-/**
- * Load plugin if core lib is present
- */
-if( ! file_exists( GMB_PLUGIN_PATH . 'vendor/wordimpress/maps-builder-core/core.php' ) ) {
-	//@TODO something better than die()
-	die();
-}else{
-	require_once GMB_PLUGIN_PATH . 'vendor/wordimpress/maps-builder-core/core.php';
-	Google_Maps_Builder_Core::include_core_classes();
-	Google_Maps_Builder();
-
-	add_action( 'widgets_init', array( Google_Maps_Builder(), 'init_widget' ) );
-}
 
