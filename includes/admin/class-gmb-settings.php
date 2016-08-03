5d9126a75e87458e32f5d716361eefc017da6e7d
Devin Walker <devin@wordimpress.com>
2016-08-03 13:00:07 -0700

    Fixed link to old support forum

--- includes/admin/class-gmb-settings.php
+++ includes/admin/class-gmb-settings.php
@@ -266,7 +266,7 @@ class Google_Maps_Builder_Settings extends Google_Maps_Builder_Core_Settings {
 	 */
 	protected function sub_heading() {
 		$out = __( 'Thank you for upgrading to Maps Builder Pro.', 'google-maps-pro' ) . ' ';
-		$out .= sprintf( __( 'As a Pro active license holder you receive %3$spriority support%2$s, awesome plugin features, and thoroughly written plugin %1$sdocumentation%2$s. We hope you enjoy using the Pro plugin version!', 'google-maps-pro' ), '<a href="https://wordimpress.com/documentation/maps-builder-pro/" target="_blank">', '</a>', '<a href="https://wordimpress.com/support/forum/maps-builder-pro" target="_blank">' );
+		$out .= sprintf( __( 'As a Pro active license holder you receive <a href="%1$s" target="_blank">priority support</a>, awesome plugin features, and thoroughly written plugin <a href="%2$s" target="_blank">documentation</a>. We hope you enjoy using the Pro plugin version!', 'google-maps-pro' ), 'https://wordimpress.com/documentation/maps-builder-pro/', 'https://wordimpress.com/support/' );
 
 		return $out;
 
