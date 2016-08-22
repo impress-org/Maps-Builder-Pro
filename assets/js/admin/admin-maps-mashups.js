009a81837e809650472b4460157dd457a949504e
Lucas Cherkewski <lucas@ecustom.ca>
2016-08-22 13:03:13 -0400

    refer to new infowindow function

--- assets/js/admin/admin-maps-mashups.js
+++ assets/js/admin/admin-maps-mashups.js
@@ -81,9 +81,7 @@ var gmb_mashup;
     app.load_configured_mashups = function () {
 
         //InfoBubble - Contains the place's information and content
-        info_bubble = new google.maps.InfoWindow({
-            maxWidth: 315
-        });
+        info_bubble = new GMB_InfoWindow();
 
         //Loop through mashup groups
         $('#gmb_mashup_group_repeat').find('.cmb-repeatable-grouping').each(function (index, value) {
