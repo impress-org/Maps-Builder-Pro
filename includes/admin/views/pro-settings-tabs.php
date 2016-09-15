dd4599245a7dc6fa9a4dc2dabfc4e54d6c427f67
Devin Walker <devin@wordimpress.com>
2016-09-14 17:25:26 -0700

    Stop using $plugin_slug https://github.com/WordImpress/Google-Maps-Builder/issues/220

--- includes/admin/views/pro-settings-tabs.php
+++ includes/admin/views/pro-settings-tabs.php
@@ -10,5 +10,5 @@
  */
 ?>
 <a href="?post_type=google_maps&page=<?php echo $key; ?>&tab=license" class="nav-tab <?php echo $active_tab == 'license' ? 'nav-tab-active' : ''; ?>">
-	<?php _e( 'License', $plugin_slug ); ?>
+	<?php _e( 'License', 'google-maps-builder' ); ?>
 </a>
